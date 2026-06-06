7468eda06f73e0243ed8503997b0e61db5438320
Ténébreux <tbagxy@gmail.com>
2026-06-05 22:12:58 -0400

    Shunter Shiny Odds Update

--- src/rogue_controller.c
+++ src/rogue_controller.c
@@ -264,11 +264,11 @@ u16 Rogue_GetShinyOdds(u8 shinyRoll)
     switch (shinyRoll)
     {
     case SHINY_ROLL_DYNAMIC:
-        baseOdds = 400;
+        baseOdds = 30000;
         break;
     
     case SHINY_ROLL_STATIC:
-        baseOdds = 100;
+        baseOdds = 8192;
         break;
     
     case SHINY_ROLL_SHINY_LOCKED:
